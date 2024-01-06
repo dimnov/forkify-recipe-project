246fbc3326543e3b93fab4335d3a4ede66e8aa80
dimnov <dimnov1512@gmail.com>
2024-01-06 14:35:17 +0200

    Change to "protected"

--- src/js/views/searchView.js
+++ src/js/views/searchView.js
@@ -1,8 +1,22 @@
 class SearchView {
-  #parentEl = document.querySelector('.search');
+  _parentEl = document.querySelector('.search');
 
   getQuery() {
-    return this.#parentEl.querySelector('.search__field').value;
+    const query = this._parentEl.querySelector('.search__field').value;
+    this._clearInput();
+
+    return query;
+  }
+
+  _clearInput() {
+    return this._parentEl.querySelector('.search__field').value = '';
+  }
+
+  addHandlerSearch(handler) {
+    this._parentEl.addEventListener('submit', (e) => {
+      e.preventDefault();
+      handler();
+    });
   }
 }
 
