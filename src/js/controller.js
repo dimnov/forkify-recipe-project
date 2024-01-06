d8d86e4a13802604723006fd405af2268d081718
dimnov <dimnov1512@gmail.com>
2024-01-07 00:24:06 +0200

    Create pagination function and buttons

--- src/js/controller.js
+++ src/js/controller.js
@@ -2,6 +2,7 @@ import { state, loadRecipe, loadSearchResults, getSearchResultsPage } from './mo
 import recipeView from './views/recipeView.js';
 import searchView from './views/searchView.js';
 import resultsView from './views/resultsView.js';
+import paginationView from './views/paginationView.js';
 
 // if (module.hot) {
 //   module.hot.accept();
@@ -32,10 +33,17 @@ const controlSearchResults = async () => {
     await loadSearchResults(query);
 
     resultsView.render(getSearchResultsPage());
+    paginationView.render(state.search);
   } catch (error) {
     throw error;
   }
 }
 
+const controlPagination = (goToPage) => {
+  resultsView.render(getSearchResultsPage(goToPage));
+  paginationView.render(state.search);
+}
+
 recipeView.addHandlerRender(controlRecipes);
 searchView.addHandlerSearch(controlSearchResults);
+paginationView.addHandlerClick(controlPagination);
