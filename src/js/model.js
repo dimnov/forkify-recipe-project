c2613df3b4d1ab89db4a1f29e136091fb2bf15e9
dimnov <dimnov1512@gmail.com>
2024-01-21 22:53:20 +0200

    bookmarks are being stored to the local storage

--- src/js/model.js
+++ src/js/model.js
@@ -71,12 +71,18 @@ export const updateServings = (newServings) => {
   state.recipe.servings = newServings;
 }
 
+const persistBookmarks = () => {
+  localStorage.setItem('bookmarks', JSON.stringify(state.bookmarks));
+}
+
 export const addBookmark = (recipe) => {
   state.bookmarks.push(recipe);
 
   if (recipe.id === state.recipe.id) {
     state.recipe.bookmarked = true;
   }
+
+  persistBookmarks();
 }
 
 export const deleteBookmark = (id) => {
@@ -86,4 +92,6 @@ export const deleteBookmark = (id) => {
   if (id === state.recipe.id) {
     state.recipe.bookmarked = false;
   }
+
+  persistBookmarks();
 }
